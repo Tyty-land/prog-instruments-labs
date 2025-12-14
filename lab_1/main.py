69583799db978afc7eb036bbe556b5a2bc993b1b
papa_A <ss-200909@mail.ru>
2025-12-14 18:06:36 +0400

    Выбор кода для проверки на соответствие правилам PEP8. Взял код лабораторной работы №4 по Прикладному программированию с позапрошлого семестра(там в общей сложности 290 строк, извините если мало, чуть не хватило)

---
+++ lab_1/main.py
@@ -0,0 +1,51 @@
+import argparse
+
+from tools_dataframe_module import writer_csv, pandas_statistical_calculation, demonstration_of_results
+from tools_imgs_module import create_absolut_dir, get_data_imgs, display_histogram
+from tools_lab_module import data_frame_filter, sort_square_images
+
+from icrawler.builtin import GoogleImageCrawler
+from typing import Tuple
+def Det_p()->Tuple[str, str, str]:
+    """
+     A function that accepts command-line parameters, namely: -k = keyword to search for a photo (default = "None")-sd = path to the future folder with photos (default = "")
+        -D = path and name to the .csv DataFrame (default = "DataFrame.csv")then collects these parameters into a tuple and returns this tuple
+    :return res_tuple: a tuple containing all three cmd parameters
+    """
+    p_cmd= argparse.ArgumentParser()
+    p_cmd.add_argument("-k", "--keyword", type=str, help="keyword", default="None")
+    p_cmd.add_argument("-sd", "--save_dir", type=str, help="save dir", default="")
+    p_cmd.add_argument("-D", "--data_frame", type=str, help="data frame file", default="DataFrame.csv")
+    args =p_cmd.parse_args()
+    res_tuple=(args.keyword, create_absolut_dir(args.save_dir), create_absolut_dir(args.data_frame))
+    return res_tuple
+
+
+
+def main() -> None:
+    """
+    The main function that performs the basic logic of the program.
+    Some functions are designed to output sequential changes to the data in the DataFrame,
+     as well as statistical data obtained by Pandas
+    :return None:
+    """
+    key_word,save_dir,data_frame =get_p()
+    google_crawl = GoogleImageCrawler(storage={'root_dir': f'{save_dir}image_{key_word}_dir'})
+    google_crawl.crawl(keyword=key_word, max_num=10)
+    writer_csv(get_data_imgs(f'{save_dir}image_{key_word}_dir'), data_frame, 0)
+    print("\n[@] - Statistical data obtained using Pandas:\n")
+    pd_st=pandas_statistical_calculation(data_frame)
+    for i in range(len(pd_st)):
+        for j in range(len(pd_st[i])):
+            print(pd_st[i][j], "\n")
+    print("[@] - The initial version of the DataFrame:")
+    demonstration_of_results(data_frame)
+    data_frame_filter(data_frame, int(pd_st[0][5]), int(pd_st[1][5]))
+    print("[@] - Filtered by DataFrame assignment:")
+    demonstration_of_results(data_frame)
+    sort_square_images(data_frame)
+    print("[@] - Sorted by the added column of DataFrame areas:")
+    demonstration_of_results(data_frame)
+    display_histogram(data_frame, 5)
+if __name__=='__main__':
+    main()
---
+++ lab_1/tools_lab_module.py
@@ -0,0 +1,69 @@
+import os
+
+from tools_dataframe_module import reader_csv, writer_csv
+from typing import List
+
+
+def data_frame_filter(data_frame: str, height_max: int, width_max: int) -> None:
+    """
+    A function from the task that filters the data in the DataFrame by
+    the width and height of the images. Overwrite the entire file with the DataFrame,
+    that is, creates a new DataFrame that will already be filtered
+    :param data_frame: The path or name to the DataFrame (.csv)
+    :param height_max: Information from Pandas about the maximum height
+    :param width_max: Information from Pandas about the maximum width
+    :return None:
+    """
+    data_imgs = reader_csv(data_frame)
+    os.remove(data_frame)
+    i = 1
+    end = len(data_imgs)
+    while i < end:
+        if int(data_imgs[i][2]) > height_max or int(data_imgs[i][3]) > width_max:
+            data_imgs.pop(i)
+            end -= 1
+        else:
+            i += 1
+    writer_csv(data_imgs, data_frame, 0)
+
+
+def sort_square_images(data_frame: str) -> None:
+    """
+    The function adds a new column of information about each image - the area of the image.
+    At the same time, it sorts from a smaller area to a larger one. The DataFrame is being overwritten
+    :param data_frame: The path or name to the DataFrame (.csv)
+    :return None:
+    """
+    data_imgs = reader_csv(data_frame)
+    os.remove(data_frame)
+    data_imgs[0].append("Square:")
+    for i_row in range(1, len(data_imgs)):
+        data_imgs[i_row].append(str(int(data_imgs[i_row][2]) * int(data_imgs[i_row][3])))
+    writer_csv(sort_data(data_imgs, 5), data_frame, 0)
+
+
+def sort_data(data_imgs: List[list], i_sort_param: int) -> List[list]:
+    """
+    A function for sorting a two-dimensional array by parameter.
+    Taking the second index of the parameter,
+    it is fully sorted from the smallest to the largest,
+    counting from the beginning of the list
+    :param data_imgs: List of image data
+    :param i_sort_param: The second index of the list item
+    :return data_imgs: List of image data (SORT)
+    """
+    x = 2
+    while x < len(data_imgs):
+        if int(data_imgs[x][i_sort_param]) < int(data_imgs[x - 1][i_sort_param]):
+            y = x
+            while int(data_imgs[y][i_sort_param]) < int(data_imgs[y - 1][i_sort_param]):
+                tmp = data_imgs[y]
+                data_imgs[y] = data_imgs[y - 1]
+                data_imgs[y - 1] = tmp
+                y -= 1
+                if y == 1:
+                    x = 2
+                    break
+                x = y
+        x += 1
+    return data_imgs
